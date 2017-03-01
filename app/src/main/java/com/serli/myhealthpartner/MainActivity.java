0192d8b5995ccdc47727cf240e47b87ad7220efd
lamya essnoussi <l.essnoussi@gmail.com>
2017-03-01 09:36:30 +0100

    Modification of the main Acctivity of the final application

--- app/src/main/java/com/serli/myhealthpartner/MainActivity.java
+++ app/src/main/java/com/serli/myhealthpartner/MainActivity.java
@@ -8,7 +8,9 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
@@ -20,6 +22,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -29,6 +32,7 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.NumberPicker;
 import android.widget.RelativeLayout;
+import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -38,7 +42,9 @@ import com.serli.myhealthpartner.controller.ProfileController;
 import com.serli.myhealthpartner.model.AccelerometerData;
 
 import java.sql.Timestamp;
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
@@ -53,154 +59,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private MainController mainController;
     private ProfileController profileController;
 
-    private NumberPicker minutePicker;
-    private NumberPicker secondPicker;
-    private Spinner activitySpinner;
-    private Button startStopButton;
-
     private boolean acquisitionStarted;
 
     private Messenger serviceMessenger = null;
     private final Messenger messenger = new Messenger(new IncomingMessageHandler());
     private boolean serviceBound = false;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_main);
-        setSupportActionBar(toolbar);
 
-
-        mainController = new MainController(this);
-        profileController = new ProfileController(this);
-
-        int result = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE);
-        if (result != PackageManager.PERMISSION_GRANTED){
-            requestPhoneStatePermission();
-        }
-        else{
-            verifyExistingProfile();
-        }
-
-        acquisitionStarted = AccelerometerService.isRunning();
-
-        minutePicker = (NumberPicker) findViewById(R.id.minute_picker);
-        minutePicker.setMinValue(0);
-        minutePicker.setMaxValue(59);
-
-        secondPicker = (NumberPicker) findViewById(R.id.second_picker);
-        secondPicker.setMinValue(0);
-        secondPicker.setMaxValue(59);
-
-        activitySpinner = (Spinner) findViewById(R.id.activity_spinner);
-        ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.sport_activity));
-        activitySpinner.setAdapter(stringArrayAdapter);
-
-        startStopButton = (Button) findViewById(R.id.button_start_stop);
-        startStopButton.setOnClickListener(this);
-
-        autoBindService();
-
-        RelativeLayout dataLayout = (RelativeLayout) findViewById(R.id.data_list_layout);
-        dataLayout.setVisibility(RelativeLayout.INVISIBLE);
-
-        Button updateButton = (Button) findViewById(R.id.button_update);
-        updateButton.setOnClickListener(this);
-
-        Button clearButton = (Button) findViewById(R.id.button_clear);
-        clearButton.setOnClickListener(this);
-
-        // Test acquisition without button
-        if (!acquisitionStarted && profileController.getProfile() != null) {
-            mainController.startAcquisition();
-            doBindService();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.action_profile) {
-            startActivity(new Intent(this, ProfileActivity.class));
-        }
-        if (item.getItemId() == R.id.action_podometre) {
-            startActivity(new Intent(this, PodometreActivity.class));
-        }
-
-        if (item.getItemId() == R.id.action_show_data) {
-            RelativeLayout dataLayout = (RelativeLayout) findViewById(R.id.data_list_layout);
-            if (dataLayout.getVisibility() == RelativeLayout.INVISIBLE) {
-                populateDataListView();
-                dataLayout.setVisibility(RelativeLayout.VISIBLE);
-            }
-            else
-                dataLayout.setVisibility(RelativeLayout.INVISIBLE);
-        }
-        return true;
-    }
 
     @Override
     public void onClick(View view) {
-        /*if (view.getId() == R.id.button_start_stop) {
-            if (acquisitionStarted) {
-                doUnbindService();
-                controller.stopAcquisition();
-                startStopButton.setText(R.string.button_start);
-                acquisitionStarted = false;
-                displayAlertDialog();
-            } else {
-                long duration = minutePicker.getValue() * 60000L + secondPicker.getValue() * 1000L;
-                if (duration > 0) {
-                    controller.startAcquisition(duration, activitySpinner.getSelectedItemPosition());
-                    doBindService();
-                }
-            }
-        }
-        if (view.getId() == R.id.button_update || view.getId() == R.id.button_clear) {
-            if (view.getId() == R.id.button_clear) {
-                controller.DeleteAcquisition();
-            }
-            populateDataListView();
-        }*/
+
     }
 
     /**
      * Ask the controller for the accelerometer data stored in the database and display them in a
      * listView.
      */
-    private void populateDataListView() {
-        ListView listView = (ListView) findViewById(R.id.data_list_view);
-        final List<AccelerometerData> dataList = mainController.getData();
-        ArrayAdapter<AccelerometerData> dataArrayAdapter = new ArrayAdapter<AccelerometerData>(this, android.R.layout.simple_list_item_2, android.R.id.text1, dataList) {
-            @NonNull
-            @Override
-            public View getView(int position, View convertView, @NonNull ViewGroup parent) {
-                View view = super.getView(position, convertView, parent);
-                TextView text1 = (TextView) view.findViewById(android.R.id.text1);
-                TextView text2 = (TextView) view.findViewById(android.R.id.text2);
-
-                AccelerometerData data = dataList.get(position);
-
-                SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy - HH:mm:ss,S", Locale.getDefault());
-
-                text1.setText(dateFormat.format(new Timestamp(data.getTimestamp())));
-                String strX = String.format("%.2f", data.getX());
-                String strY = String.format("%.2f", data.getY());
-                String strZ = String.format("%.2f", data.getZ());
-                text2.setText("x=" + strX + " y=" + strY + " z=" + strZ + " " + MainActivity.this.getResources().getTextArray(R.array.sport_activity)[data.getActivity()]);
-
-                return view;
-            }
-        };
-        listView.setAdapter(dataArrayAdapter);
-    }
-
     @Override
     protected void onDestroy() {
         doUnbindService();
@@ -283,30 +158,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    /**
-     * Asks user if he wants to send accelerometer data acquired to the database
-     */
-    /*private void displayAlertDialog(){;
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-        alertDialogBuilder.setMessage(R.string.sending_data_message);
-        alertDialogBuilder.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                //controller.sendAcquisition(); ==> A EFFECTUER DES QUE LA VERITABLE URL DE L'ACCES AU SERVEUR EST VALIDE !
-                Toast.makeText(MainActivity.this, R.string.acquisition_sent,Toast.LENGTH_LONG).show();
-            }
-        });
-        alertDialogBuilder.setNegativeButton(R.string.no,new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                controller.DeleteAcquisition();
-                Toast.makeText(MainActivity.this, R.string.acquisition_deleted, Toast.LENGTH_LONG).show();
-            }
-        });
-
-        AlertDialog alertDialog = alertDialogBuilder.create();
-        alertDialog.show();
-    }*/
 
     //Requesting permission
     private void requestPhoneStatePermission(){
@@ -337,5 +188,226 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent intent = new Intent(this, ProfileActivity.class);
             startActivity(intent);
         }
+    }
+
+    //////////partie podometre
+    private static final String TAG = MainActivity.class.getSimpleName();
+    //  private static final String PREFERENCES_NAME = "Values";
+    private static final String PREFERENCES_VALUES_THRESHOLD_KEY = "threshold";
+    private SharedPreferences preferences;
+    private int mStepCount = 0;
+    private AccelerometerDetector mAccelDetector;
+    private TextView mThreshValTextView;
+    private TextView mStepCountTextView;
+    private TextView mTimeValTextView;
+
+
+    private TimeCounter mTimer;
+    private Handler mHandler = new Handler();
+
+    /***************/
+    // private ProfileController profController ;
+    int height;
+    int weight;
+    int gender;
+    double resultE = 0;
+    String result;
+    TextView counter;
+    TextView number_calorie;
+    double res = 0;
+    String resultCalories, resultDistance;
+    TextView counterCalories, counterDistance;
+    TextView numberCalories, distanceTravelled;
+    double resCalories = 0;
+    double resDistance = 0;
+    /***************/
+    // constant reference
+    private final AccelerometerProcessing mAccelerometerProcessing = AccelerometerProcessing.getInstance();
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
+
+        //   setContentView(R.layout.activity_podometre);
+
+        // set default locale:
+        Locale.setDefault(Locale.ENGLISH);
+        // get and configure text views
+        mThreshValTextView = (TextView)findViewById(R.id.threshval_textView);
+        formatThreshTextView(AccelerometerProcessing.THRESH_INIT_VALUE);
+        mStepCountTextView = (TextView)findViewById(R.id.stepcount_textView);
+        mStepCountTextView.setText(String.valueOf(0));
+        mTimeValTextView = (TextView)findViewById(R.id.timeVal_textView);
+
+        /****************/
+        counter = (TextView) findViewById(R.id.calorie_affiche);
+        number_calorie=(TextView)findViewById(R.id.number_calorie);
+        //dist
+        counterDistance=(TextView) findViewById(R.id.distance_affiche);
+        distanceTravelled=(TextView)findViewById(R.id.number_distance);//
+
+        profileController = new ProfileController(this);
+        height = profileController.getProfile().getHeight();
+        weight = profileController.getProfile().getWeight();
+        gender = profileController.getProfile().getGender();
+        /****************/
+
+        // timer counter
+        mTimer = new TimeCounter(mHandler,mTimeValTextView);
+        mTimer.start();
+
+        // initialize accelerometer
+        SensorManager sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        mAccelDetector = new AccelerometerDetector(sensorManager);
+        mAccelDetector.setStepCountChangeListener(new OnStepCountChangeListener() {
+            @Override
+            public void onStepCountChange(long eventMsecTime) {
+                ++mStepCount;
+                res = getCaloris()*mStepCount;
+                result = res + " cal";
+                number_calorie.setText(result);
+                mStepCountTextView.setText(String.valueOf(mStepCount));
+                ///dist
+                resDistance = (double) Math.round((calculateDistance() * mStepCount) * 100) / 100; // 4.248 --> 4.25
+                resultDistance = resDistance + " km";
+                distanceTravelled.setText(resultDistance);//
+            }
+        });
+
+        initializeSeekBar();
+
+    }
+
+    /**
+     * SeekBar is the publisher.
+     *
+     */
+    private void initializeSeekBar() {
+        final SeekBar seekBar = (SeekBar)findViewById(R.id.offset_seekBar);
+        seekBar.setMax(130 - 90);
+        seekBar.setProgress((int) AccelerometerProcessing.getInstance().getThresholdValue());
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                double threshold = AccelerometerProcessing.THRESH_INIT_VALUE * (progress + 90) / 100;
+                mAccelerometerProcessing.onThresholdChange(threshold);
+                formatThreshTextView(threshold);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    private void formatThreshTextView(double v) {
+        final DecimalFormat df = new DecimalFormat("#.##");
+        mThreshValTextView.setText(String.valueOf(df.format(v)));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_profile) {
+            startActivity(new Intent(this, ProfileActivity.class));
+        }
+        if (item.getItemId() == R.id.action_podometre) {
+            startActivity(new Intent(this, PodometreActivity.class));
+        }
+        if (item.getItemId() == R.id.action_repport) {
+            startActivity(new Intent(this, RepportActivity.class));
+        }
+        //action_repport
+       /* if (item.getItemId() == R.id.action_show_data) {
+            RelativeLayout dataLayout = (RelativeLayout) findViewById(R.id.data_list_layout);
+            if (dataLayout.getVisibility() == RelativeLayout.INVISIBLE) {
+                populateDataListView();
+                dataLayout.setVisibility(RelativeLayout.VISIBLE);
+            }
+            else
+                dataLayout.setVisibility(RelativeLayout.INVISIBLE);
+            startActivity(new Intent(this, ProfileActivity.class));
+        }*/
+        return true;
+    }
+
+    private void saveThreshold() {
+        preferences.edit().putFloat(
+                PREFERENCES_VALUES_THRESHOLD_KEY,
+                (float) AccelerometerProcessing.getInstance().getThresholdValue()).apply();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mAccelDetector.startDetector();
+
+        mTimer.resume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "OnPause");
+        mAccelDetector.stopDetector();
+        mTimer.pause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "OnStop");
+    }
+
+    public int getAge() {
+        Calendar curr = Calendar.getInstance();
+        Calendar birth = Calendar.getInstance();
+
+        birth.setTime(profileController.getProfile().getBirthday());
+        int age = curr.get(Calendar.YEAR) - birth.get(Calendar.YEAR);
+        curr.add(Calendar.YEAR, -age);
+        if (birth.after(curr)) {
+            age = age - 1;
+        }
+        return age;
+    }
+
+    public double getCaloris(){
+        if (gender == 0) {
+            resultE = height * 0.415 * 0.00001 * (weight*2.02);
+        }
+        if (gender==1) {
+            resultE = height * 0.413 * 0.00001 * (weight*2.02);
+        }
+
+        return resultE;
+    }
+
+    /**
+     * Calculates the distance travelled
+     * @return The distance travalled
+     */
+    public double calculateDistance(){
+        double distance = 0;
+        if (gender == 0) {
+            distance = height * 0.415 * 0.00001;
+        }
+        if (gender == 1) {
+            distance = height * 0.413 * 0.00001;
+        }
+        return distance;
     }
 }
