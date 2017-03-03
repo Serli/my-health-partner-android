c188e853041113bb6f6242239f7b157f32034ab5
salquier <simonalquier92@gmail.com>
2017-03-03 16:44:03 +0100

    Change call to /recognize

--- app/src/main/java/com/serli/myhealthpartner/controller/PostTo.java
+++ app/src/main/java/com/serli/myhealthpartner/controller/PostTo.java
@@ -1,25 +1,18 @@
 package com.serli.myhealthpartner.controller;
 
-import com.serli.myhealthpartner.model.AccelerometerData;
 import com.serli.myhealthpartner.model.CompleteData;
-import com.serli.myhealthpartner.model.ProfileData;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
-import retrofit2.http.GET;
 import retrofit2.http.POST;
-
-/**
- * Created by nathan on 21/12/16.
- */
 
 /**
  * Communication mobile to server
  */
 public interface PostTo {
-    @POST("/data")
-    Call<ArrayList<CompleteData>> sendData (@Body ArrayList<CompleteData> data);
+    @POST("/recognize")
+    Call<List<Long>> sendData (@Body ArrayList<CompleteData> data);
 }
